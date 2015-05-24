cd1229a0bd1b97aab9ce4f78c26692d4919f388d
Adm Masaweb <adm.masaweb@gmail.com>
2015-05-24 20:41:29 +0300

    initial commit

---
+++ styledSelect.js
@@ -0,0 +1,76 @@
+
+/*global window */
+var styledSelect = (function () {
+    'use strict';
+
+    var mainEl;
+
+    function renderHtml(cfg) {
+        var el, html, i;
+
+        html  = '<div class="styled-select-label">' + cfg.label + '</div>' +
+                '<div class="styled-select-value-list">' +
+                    '<div class="styled-select-value-wrapper">' +
+                        '<div class="styled-select-value">' + cfg.emptyValue + '</div>' +
+                        '<span class="fa fa-caret-down"></span>' +
+                    '</div>' +
+                    '<div class="styled-select-list">';
+
+        for (i = 0; i < cfg.options.length; i += 1) {
+            el = cfg.options[i];
+            html +=     '<div class="styled-select-list-item"' +
+                            ' data-value="' + el.value + '"' +
+                            ' display-value="' + el.displayValue + '">' +
+                            el.html +
+                        '</div>';
+        }
+        html +=     '</div>' +
+                '<div>';
+
+        mainEl.innerHTML = html;
+    }
+
+    function create(cfg) {
+        var  els, i;
+
+        mainEl = document.getElementById(cfg.id);
+        if (!mainEl) {
+            return;
+        }
+
+        renderHtml(cfg);
+
+        els = mainEl.getElementsByClassName('styled-select-value');
+
+        (function () {
+            var selectListField, valueField;
+
+            function clickItem(e) {
+                e.stopPropagation(); // prevent 'blur', see body click below
+                valueField.children[0].setAttribute('data-value', this.attributes['data-value'].value);
+                valueField.children[0].innerHTML = this.attributes['display-value'].value;
+                selectListField.style.display = 'none';
+            }
+
+            valueField = els[0].parentElement;
+            valueField.addEventListener('click', function (e) {
+                e.stopPropagation(); // prevent 'blur', see body click below
+                selectListField = this.parentElement.getElementsByClassName('styled-select-list')[0];
+                selectListField.style.display = (selectListField.style.display !== 'block') ? 'block' : 'none';
+            }, false);
+
+            document.getElementsByTagName('body')[0].addEventListener('click', function () {
+                selectListField.style.display = 'none';
+            }, false);
+
+            els = mainEl.getElementsByClassName('styled-select-list-item');
+            for (i = 0; i < els.length; i += 1) {
+                els[i].addEventListener('click', clickItem, false);
+            }
+        }());
+    }
+
+    return {
+        create: create
+    };
+}());
